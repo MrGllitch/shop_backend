2bd9b4f8996e3057c48edf0d062940a19fbeabce
Matěj K <hobitekCZ@post.cz>
2023-07-04 23:54:57 +0200

    images preview and add design

--- shop_backend/src/routes/(admin)/add/+page.svelte
+++ shop_backend/src/routes/(admin)/add/+page.svelte
@@ -2,6 +2,15 @@
 
     let categories=[]
     let colors=[]
+    let files : any = []; 
+
+function onChange(event : any) {
+  const fileList = event.target.files;
+  files = [...files, ...Array.from(fileList)];
+}
+function removeFile(index : number) {
+    files = files.filter((_: any, i: any) => i !== index);
+  }
 </script>
 
 <style>
@@ -12,12 +21,18 @@
       padding: 20px;
       border-radius: 5px;
       box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-      max-width: 66.6%;
-      margin: 0 auto;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
+      max-width: 85%;
+      margin: 0 auto; 
+      display: grid;
+      grid-template-areas:
+      "name price owner"
+      "categories colors discount"
+      "location dimensions description"
+      "images images images "
+      "date note submit";
+      column-gap: 10px;
+      row-gap: 5em;
+
     }
   
     label {
@@ -43,77 +58,162 @@
       border: none;
       border-radius: 3px;
       cursor: pointer;
-      margin-top: 10px;
     }
   
     button[type="submit"] {
       background-color: #008CBA;
     }
 
-    .groups{
-        display: flex;
+
+    .preview{
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      align-items: center;
+    }
+    .image_card {
+    display: flex;
+    flex-direction: column;
+    border: 1px solid black;
+    }
+    .image_card img{
+      max-width: 16em;
+      max-height: 16em;
+      object-fit: scale-down;
+    }
+    .remove{
+      background-color: #fff;
+      padding: 0.2em 0.2em;
+      font-size: 1.3em;
+      color: red;
+    }
+    .head{
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+
+    .name{
+      grid-area: name;
+    }
+    .price{
+      grid-area: price;
+    }
+    .owner{
+      grid-area: owner;
+    }
+    .note{
+      grid-area: note;
+    }
+    #categories{
+      grid-area: categories;
+    }
+    #colors{
+      grid-area: colors;
+    }
+    #discount{
+      grid-area: discount;
+    }
+    #location{
+      grid-area: location;
+    }
+    #dimensions{
+      grid-area: dimensions;
+    }
+    .description{
+      grid-area: description;
+    }
+    .images{
+      grid-area: images;
+    }
+    .date{
+      grid-area: date;
+    }
+    #submit{
+      grid-area: submit;
     }
   </style>
   
 
   <form>
-    <label for="name">Name</label>
-    <input type="text" id="name" name="name">
+
+  <!-- 1. -->
+    <label class="name" for="name">Name</label>
+    <input class="name" type="text" id="name" name="name">
+
+    <label class="price" for="price">Price</label>
+    <input class="price" type="number" id="price" name="price">
+
+    <label class="owner" for="owner">Owner</label>
+    <input class="owner" type="text" id="owner" name="owner">
+
+    <label class="note" for="note">Note</label>
+    <input class="note" type="text" id="note" name="note">
+
+  <!-- 2. -->
+
+    <button id="categories">Select categories</button>
+    <button id="colors">Select colors</button>
+    <button id="discount">Create discount</button>
   
-    <label for="price">Price</label>
-    <input type="number" id="price" name="price">
-  
-    <label for="images">Images</label>
-    <input type="file" id="images" name="images" multiple>
-  
-    <section class="groups">
-    <fieldset>
-      <legend>Location</legend>
-  
-      <label for="locality">Locality</label>
-      <input type="text" id="locality" name="locality">
-  
-      <label for="region">Region</label>
-      <input type="text" id="region" name="region">
-  
-      <label for="state">State</label>
-      <input type="text" id="state" name="state">
-    </fieldset>
-  
-    <fieldset>
-      <legend>Dimensions (in cm)</legend>
-  
-      <label for="width">Width</label>
-      <input type="number" id="width" name="width">
-  
-      <label for="height">Height</label>
-      <input type="number" id="height" name="height">
-  
-      <label for="depth">Depth</label>
-      <input type="number" id="depth" name="depth">
-    </fieldset>
-    </section>
+  <!-- 3. -->
+
+  <fieldset id="location">
+    <legend>Location</legend>
+
+    <label for="locality">Locality</label>
+    <input type="text" id="locality" name="locality">
+
+    <label for="region">Region</label>
+    <input type="text" id="region" name="region">
+
+    <label for="state">State</label>
+    <input type="text" id="state" name="state">
+  </fieldset>
+
+  <fieldset id="dimensions">
+    <legend>Dimensions (in cm)</legend>
+
+    <label for="width">Width</label>
+    <input type="number" id="width" name="width">
+
+    <label for="height">Height</label>
+    <input type="number" id="height" name="height">
+
+    <label for="depth">Depth</label>
+    <input type="number" id="depth" name="depth">
+
     <label for="weight">Weight</label>
     <input type="number" id="weight" name="weight">
+  </fieldset>
   
-    <label for="description">Description</label>
-    <input type="text" id="description" name="description">
+  <label class="description" for="description">Description</label>
+  <input class="description" type="text" id="description" name="description">
+
+  <!-- 4. -->
+
+    <label class="images" for="images">Images</label>
+    <input class="images" type="file" id="images" name="images" multiple on:change={onChange}>
+    <div class="preview images">
+    {#each files as file, index}
+    <div class="image_card images">
+      <div class="head">      
+        <span>{file.name}</span>
+        <button class="remove" on:click={() => removeFile(index)}>X</button>
+      </div>
+
+      <img src={URL.createObjectURL(file)} alt="Preview" />
+    </div>
+  {/each}
+  </div>
+      
+  <!-- 5. -->
   
-    <h3>Select others</h3>
-    <button>Create discount</button>
-    <button>Select colors</button>
-    <button>Select categories</button>
+    <label class="date" for="date">Publish selected:</label>
+    <input class="date" type="date" id="date" name="date">
   
-    <label for="date">Publish selected:</label>
-    <input type="date" id="date" name="date">
-  
-    <button type="submit">submit</button>
+    <button id="submit" type="submit">submit</button>
   </form>
   
   
 
-<!-- 
-discounts Discount[]
-colors Color[]
-categories Category[]
-publishedAt DateTime? -->
